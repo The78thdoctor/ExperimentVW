a4c83fefd01670e16e7c49c0e4e1ac05ee5ff1e2
Doctor <45296988+The78thdoctor@users.noreply.github.com>
2019-02-06 21:06:19 -0500

    Update playing.js

--- util/playing.js
+++ util/playing.js
@@ -1,11 +1,11 @@
 module.exports = (bot) => {
-    bot.user.setActivity(`Controller of all Tardis Bots`, { type: "ONLINE", url: "https://www.twitch.tv/elarabots_discord" });
+    bot.user.setActivity(`Controller of all Tardis Bots`, { type: "PLAYING", url: "https://www.twitch.tv/elarabots_discord" });
     var status = [
         `CONTROLLER OF ALL BOTS`
     ];
     setInterval(() => {
         let gameval = Math.floor((Math.random() * status.length));
-        bot.user.setActivity(`${status[gameval]}`, { type: "ONLINE", url: "https://www.twitch.tv/elarabots_discord" });
+        bot.user.setActivity(`${status[gameval]}`, { type: "PLAYING", url: "https://www.twitch.tv/elarabots_discord" });
     }, 10 * 1000);
 }
 // This is to set your bot's playing status it will randomize through the statuses
